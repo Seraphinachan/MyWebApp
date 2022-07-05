eb36e3c2fee28705fd371334c4f4a48755ca69d8
김지연 <jiyeonyee0312@gmail.com>
2022-07-05 14:09:17 +0900

    "Hello, World!"

---
+++ src/test/java/com/mycompany/MyWebApp/UserRepositoryTests.java
@@ -0,0 +1,32 @@
+package com.mycompany.MyWebApp;
+
+import com.mycompany.MyWebApp.user.User;
+import com.mycompany.MyWebApp.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew() {
+        User user = new User();
+        user.setEmail("jiyoenyee0312@gmail.com");
+        user.setPassword("1111");
+        user.setFirstName("Jiyeon");
+        user.setLastName("Kim");
+
+        User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+}
